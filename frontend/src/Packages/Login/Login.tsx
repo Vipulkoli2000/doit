19b23099479734e4d14ef01bd0c66535e02c875f
Vipulkoli2000 <vipulkoli2323@gmail.com>
2024-09-21 17:55:34 +0530

    vvv

--- frontend/src/Packages/Login/Login.tsx
+++ frontend/src/Packages/Login/Login.tsx
@@ -53,12 +53,16 @@ const Login = () => {
               d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"
             />
           </svg>
-          Logo
+          DOIT
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
-            <p className="text-lg text-white font-bold">Welcome To Website</p>
-            <footer className="text-sm text-white">Sofia Davis</footer>
+            <p className="text-lg text-white font-bold">
+              Welcome To Task Manager
+            </p>
+            <footer className="text-sm text-white">
+              Sanmisha Technologies
+            </footer>
           </blockquote>
         </div>
       </div>
