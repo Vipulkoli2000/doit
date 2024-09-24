fccdc49f6180a8d316a4c4e0d82b307546a2542e
Vipulkoli2000 <vipulkoli2323@gmail.com>
2024-09-24 13:15:56 +0530

    vv

--- frontend/src/Pages/TaskManager/Task.tsx
+++ frontend/src/Pages/TaskManager/Task.tsx
@@ -374,14 +374,14 @@ export function DataTableDemo() {
 
       <div className="w-full py-4 px-4">
         <Sheet>
-          <SheetTrigger>
+          <SheetTrigger className="block sm:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-6"
+              className="size-6 "
             >
               <path
                 strokeLinecap="round"
--- frontend/src/Pages/Users.tsx
+++ frontend/src/Pages/Users.tsx
@@ -226,7 +226,7 @@ export function DataTableDemo() {
 
       <div className="w-full py-4 px-4">
         <Sheet>
-          <SheetTrigger>
+          <SheetTrigger className="block sm:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
